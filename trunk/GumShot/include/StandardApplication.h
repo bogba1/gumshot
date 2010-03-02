92b308179250490f69e4bdf0390580cd16b9cd3e
lilrobzn@gmail.com <lilrobzn@gmail.com@4458f286-260b-11df-a76c-adfa62dee608>
2010-03-02 19:52:05 +0000

    Setting Base; Ignoring Files

---
+++ trunk/GumShot/include/StandardApplication.h
@@ -0,0 +1,46 @@
+#ifndef __StandardApplication_H__
+#define __StandardApplication_H__
+
+#include "OGRE/Ogre.h"
+#include "OGRE/OgreConfigFile.h"
+#include "StandardFrameListener.h"
+#include "Resource.h"
+
+using namespace Ogre;
+
+/** Base class which manages the standard startup of an Ogre application.
+    Designed to be subclassed for specific examples if required.
+*/
+class StandardApplication
+{
+	public:
+		StandardApplication();
+		virtual ~StandardApplication();
+	
+	//Methods:
+		virtual void				go(void);
+	protected:  
+		virtual bool				setup(void);
+		virtual bool				configure(void);
+		virtual void				chooseSceneManager(void);
+		virtual void				createCamera(void);
+		virtual void				createFrameListener(void);
+		virtual void				createScene(void) = 0;
+		virtual void				createLights(void) = 0;
+		virtual void				destroyScene(void);
+		virtual void				createViewports(void);
+		virtual void				setupResources(void);
+		virtual void				createResourceListener(void);
+		virtual void				loadResources(void);
+
+	//Attributes:
+		Root*						_root;
+		Camera*						_camera;
+		SceneManager*				_sceneManager;
+		StandardFrameListener*		_frameListener;
+		RenderWindow*				_window;
+		Ogre::String				_resourcePath;
+};
+
+
+#endif
---
+++ trunk/GumShot/include/StandardFrameListener.h
@@ -0,0 +1,69 @@
+#ifndef __StandardFrameListener_H__
+#define __StandardFrameListener_H__
+
+#include "OGRE/Ogre.h"
+#include "OGRE/OgreStringConverter.h"
+#include "OGRE/OgreException.h"
+
+//Use this define to signify OIS will be used as a DLL
+//(so that dll import/export macros are in effect)
+#define OIS_DYNAMIC_LIB
+#include <OIS/OIS.h>
+
+using namespace Ogre;
+
+class StandardFrameListener: public FrameListener, public WindowEventListener
+{
+	public:
+		// Constructor takes a RenderWindow because it uses that to determine input context
+		StandardFrameListener(RenderWindow* win, Camera* cam, bool bufferedKeys = false, bool bufferedMouse = false, bool bufferedJoy = false );
+		virtual ~StandardFrameListener();
+		
+		//Methods:
+		virtual void				windowResized(RenderWindow* rw);//Adjust mouse clipping area
+		virtual void				windowClosed(RenderWindow* rw);//Unattach OIS before window shutdown (very important under Linux)
+		virtual bool				processUnbufferedKeyInput(const FrameEvent& evt);
+		virtual bool				processUnbufferedMouseInput(const FrameEvent& evt);
+		virtual void				moveCamera();
+		virtual void				showDebugOverlay(bool show);
+		bool						frameRenderingQueued(const FrameEvent& evt);// Override frameRenderingQueued event to process that (don't care about frameEnded)
+		bool						frameEnded(const FrameEvent& evt);
+		
+	protected:
+		virtual void				updateStats(void);
+
+		//Attributes:
+	protected:
+		Camera*						mCamera;
+
+		Vector3						mTranslateVector;
+		Real						mCurrentSpeed;
+		RenderWindow*				mWindow;
+		bool						mStatsOn;
+
+		std::string					mDebugText;
+
+		unsigned int				mNumScreenShots;
+		float						mMoveScale;
+		float						mSpeedLimit;
+		Degree						mRotScale;
+
+		//to stop toggles flipping too fast
+		Real						mTimeUntilNextToggle ;
+		Radian						mRotX, mRotY;
+		TextureFilterOptions		mFiltering;
+		int							mAniso;
+
+		int							mSceneDetailIndex ;
+		Real						mMoveSpeed;
+		Degree						mRotateSpeed;
+		Overlay*					mDebugOverlay;
+
+		//OIS Input devices
+		OIS::InputManager*			mInputManager;
+		OIS::Mouse*					mMouse;
+		OIS::Keyboard*				mKeyboard;
+		OIS::JoyStick*				mJoy;
+};
+
+#endif
