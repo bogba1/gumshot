25f1bc2389d8906f9fe8bcdae704e9915ddf76f5
lilrobzn@gmail.com <lilrobzn@gmail.com@4458f286-260b-11df-a76c-adfa62dee608>
2010-03-02 21:49:17 +0000

    material + mesh added

--- trunk/GumShot/src/OgreApplication.cpp
+++ trunk/GumShot/src/OgreApplication.cpp
@@ -41,17 +41,12 @@ void OgreApplication::createScene()
 	//here you can create entities, lights and other stuff
 	_sceneManager->setShadowTechnique(ShadowTechnique::SHADOWTYPE_STENCIL_ADDITIVE);
 	_sceneManager->setShadowFarDistance(1000);
-	Entity* _entity = _sceneManager->createEntity("cube_ding", "cube.mesh");
+	Entity* _entity = _sceneManager->createEntity("gs_ball_machine", "gs_ball_machine.mesh");
 	_entity->setCastShadows(true);
-	SceneNode* _node = _sceneManager->getRootSceneNode()->createChildSceneNode("cubeDingNode"); //_sceneManager->createSceneNode("nodeHallo");
+	SceneNode* _node = _sceneManager->getRootSceneNode()->createChildSceneNode("gs_ball_machine_node"); //_sceneManager->createSceneNode("nodeHallo");
 	_node->attachObject(_entity);
 	_node->setPosition(0,0,0);
 
-	_entity = _sceneManager->createEntity("cube", "cube.mesh");
-	_entity->setCastShadows(false);
-	_node = _sceneManager->getRootSceneNode()->createChildSceneNode("cubeNode");
-	_node->attachObject(_entity);
-	_node->setPosition(0,0,0);
 
 	CollisionPrimitives::TreeCollisionSceneParser* _colShape = new CollisionPrimitives::TreeCollisionSceneParser(_world);
 	_colShape->parseScene(_node,1);
@@ -59,6 +54,18 @@ void OgreApplication::createScene()
 	_body->attachNode(_node); //attaching level node to rigidbody(collision shaped)
 	_body->setPositionOrientation(Ogre::Vector3(0, 0, 0), Ogre::Quaternion::IDENTITY);
 
+	Ogre::MovablePlane* _plane = new Ogre::MovablePlane("Plane");
+	_plane->d = 0;
+	_plane->normal = Vector3::UNIT_Y;
+		Ogre::MeshManager::getSingleton().createPlane("planeMesh",
+		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+	*_plane, 80, 80, 20, 20, true, 1, 3, 3, Vector3::UNIT_Z);
+	Entity *_planeEnt = _sceneManager->createEntity("plane", "planeMesh");
+	_planeEnt->setCastShadows(true);
+	_planeEnt->setMaterialName("planeMat");
+	_node = _sceneManager->getRootSceneNode()->createChildSceneNode("planeNode");
+	_node->attachObject(_planeEnt);
+	_node->setPosition(0,0,0);
 }
 
 //------------------------------------------------------
@@ -78,33 +85,14 @@ void OgreApplication::createLights()
 	//directional yellow light
 	Light* _light = _sceneManager->createLight("light1");
 	_light->setType(Ogre::Light::LightTypes::LT_DIRECTIONAL);
-	_light->setDiffuseColour(ColourValue(.25, .25, 0));
-    _light->setSpecularColour(ColourValue(.25, .25, 0));
+	_light->setDiffuseColour(ColourValue(1, 1, 1));
+    _light->setSpecularColour(ColourValue(1, 1, 1));
 	_light->setDirection(0,-1,1);
 	_light->setCastShadows(true);
 
-	//light clamped to camera
-	_light = _sceneManager->createLight("light2");
-	_light->setType(Ogre::Light::LightTypes::LT_SPOTLIGHT);
-	_light->setPosition(0,0,30);
-	_light->setDiffuseColour(1,1,1);
-	_light->setSpecularColour(.8,1,.8);
-	_light->setAttenuation(100,1,0.005,0);
-	_light->setSpotlightRange(Degree(35), Degree(50));
-	_light->setCastShadows(true);
-
-	//light rotating around cube
-	_light = _sceneManager->createLight("light3");
-	_light->setType(Light::LightTypes::LT_POINT);
-	_light->setPosition(5,1,0);
-	_light->setDiffuseColour(.5,.5,.5);
-	_light->setSpecularColour(.3,.5,1);
-	_light->setAttenuation(2,1,0,0);
-	_light->setCastShadows(true);
 
 	SceneNode* _node = _sceneManager->getRootSceneNode()->createChildSceneNode("pLightNode");
 	_node->attachObject(_light);
 	_node->setPosition(0,0,0);
-
 
 }
