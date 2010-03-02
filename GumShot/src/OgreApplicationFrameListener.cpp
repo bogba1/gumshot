703b813fdd071c73d802a105b7b80ee0a7e55083
lilrobzn <lilrobzn@gmail.com>
2010-03-02 21:49:17 +0000

    material + mesh added

--- GumShot/src/OgreApplicationFrameListener.cpp
+++ GumShot/src/OgreApplicationFrameListener.cpp
@@ -16,21 +16,10 @@ bool OgreApplicationFrameListener::frameStarted(const FrameEvent& evt)
 	double _dt = evt.timeSinceLastFrame;
 
 	//rotating cube dings
-	Node* _node = _sceneManager->getRootSceneNode()->getChild("cubeDingNode");
+	Node* _node = _sceneManager->getRootSceneNode()->getChild("gs_ball_machine_node");
 	Vector3 _vec = Vector3(.2+sin(_timer), cos(_timer), .4+sin(_timer));
-	_node->rotate(_vec,Radian(45*(Math::PI/180)) * _dt);
+	//_node->rotate(_vec,Radian(45*(Math::PI/180)) * _dt);
 
-	//rotating cube
-	_node = _sceneManager->getRootSceneNode()->getChild("cubeNode");
-	_node->roll(Radian(9*(Math::PI/180)) * _dt); //rotating 9°/sec z-axis
-	_node->pitch(Radian(4.5*(Math::PI/180)) * _dt); //rotating 4,5°/sec x-axis
-	_node->yaw(Radian(18*(Math::PI/180)) * _dt); //rotating 18°/sec y-axis
-
-	//rotate pLight around cube
-	_node = _sceneManager->getRootSceneNode()->getChild("pLightNode");
-	_node->rotate(Vector3(0,1,0), Radian(180*(Math::PI/180))* _dt);
-	_vec = _node->getPosition();
-	_node->setPosition(_vec * Vector3(0,sin(_timer),0));
 
 	//does not work with 1.7 SDK
 	//StandardFrameListener::updateStats();
@@ -41,11 +30,7 @@ bool OgreApplicationFrameListener::frameStarted(const FrameEvent& evt)
 
 bool OgreApplicationFrameListener::frameEnded(const FrameEvent& evt)
 {
-	//setting light to camera position and direction to center
-	Light* _light = _sceneManager->getLight("light2");
-	Vector3 _vec = Vector3(_cam->getPosition().x, _cam->getPosition().y, _cam->getPosition().z);
-	_light->setPosition(_vec);
-	_light->setDirection(Vector3(0,0,0) - _vec);
+
 	return true;	
 }
 
